6f09b66e19420a8e8fa04822e6359b27f288de14
dufolk <1589768970@qq.com>
2023-11-09 21:51:02 +0800

    代码结构优化

--- src/services/command_publish_service.py
+++ src/services/command_publish_service.py
@@ -4,7 +4,7 @@ import socketserver
 # 用于发布比赛命令的服务
 
 class CommandPublishService():
-    def publish(self,command:str):
+    def publish(command:str):
         msg = command.encode('utf-8')
         for client in GlobalStatus.Clients:
-            client.sendall(msg)
+            client.entity.sendall(msg)
--- src/services/server_service.py
+++ src/services/server_service.py
@@ -2,19 +2,40 @@ import socketserver
 import json
 from ..utils import GlobalStatus
 from . import CommandPublishService
+from ..models import ClientEntity
 import time 
+import select
 
 # Author ： ZP
 # 用于处理客户端的请求
 class ServerService(socketserver.BaseRequestHandler):                              
     def handle(self):
         print('Got connection from', self.client_address)
-        GlobalStatus.Clients.append(self.request)
-        while self.request.recv(1):
-            self.command_publish("Hello")
-            time.sleep(1)
+        id = self.myrecv()
+        currclient = ClientEntity(id, self.request)
+        GlobalStatus.Clients.append(currclient)
+        timeout = 100
+        while True:
+            rlist, _, _ = select.select([self.request], [], [], timeout)
+            if not rlist:
+                break
+            else:
+                data = self.myrecv()
+        
+        print('Lost connection from', self.client_address)
+        GlobalStatus.Clients.remove(currclient)
 
     def command_publish(self, command):
         CommandPublishService.publish(command)
+
+    def myrecv(self):
+        try:
+            length = int(self.request.recv(4).decode('utf-8'))
+        except:
+            self.request.recv(1024)
+            return None
+        data = self.request.recv(length)
+        data = self.mydecoder(data)
+        return data
 
         
