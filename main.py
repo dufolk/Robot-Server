996b0aab82aa0b6b7dc2ea1298d1714dbf952f6e
ren <renyuankuo@163.com>
2023-11-09 20:37:27 +0800

    发布命令代码

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 from src.services import ServerService
 from src.services import LocationPubService
+from src.services import CommandPublishService
 import socketserver
 
 if __name__ == '__main__':
 
 
 
-    # s=ServerService()
-    # s.publish(123)
 
-
-
-
-    host = '192.168.1.8'
+    host = '192.168.1.12'
     port = 9999
     server = socketserver.ThreadingTCPServer((host, port), ServerService)
     print('server started')
--- src/services/server_service.py
+++ src/services/server_service.py
@@ -2,18 +2,19 @@ import socketserver
 import json
 from ..utils import GlobalStatus
 from . import CommandPublishService
+import time 
 
 # Author ： ZP
 # 用于处理客户端的请求
 class ServerService(socketserver.BaseRequestHandler):                              
     def handle(self):
         print('Got connection from', self.client_address)
-        clienteneity = 1
         GlobalStatus.Clients.append(self.request)
-        print(GlobalStatus.Clients)
+        while self.request.recv(1):
+            self.command_publish("Hello")
+            time.sleep(1)
 
-class RobotServer:
-    def commandpublish(self, command):
+    def command_publish(self, command):
         CommandPublishService.publish(command)
 
         
