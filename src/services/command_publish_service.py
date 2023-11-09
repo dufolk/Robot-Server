17992d401d70f560e8fc7f5635551324cf9f6f9e
ren <renyuankuo@163.com>
2023-11-09 20:37:27 +0800

    发布命令代码

--- src/services/command_publish_service.py
+++ src/services/command_publish_service.py
@@ -1,6 +1,10 @@
 from ..utils import GlobalStatus
+import socketserver
 # Author : ryk
 # 用于发布比赛命令的服务
-class CommandPublishService:
-    def publish():
-        pass
+
+class CommandPublishService():
+    def publish(self,command:str):
+        msg = command.encode('utf-8')
+        for client in GlobalStatus.Clients:
+            client.sendall(msg)
