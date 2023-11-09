70e86d3ec8e937877f74f65660f030fc3fc5e70d
yhy <362316907@qq.com>
2023-11-09 21:51:06 +0800

    client

--- tests/models/client_model.py
+++ tests/models/client_model.py
@@ -1,2 +1,26 @@
+# Author : YU
+# 用于客户端功能类定义
+import socket
+import sys
+sys.path.append('.')
+from src.utils import *
+
 class ClientModel:
-    pass
+    def __init__(self, server_ip, server_port):
+        self.server_host = server_ip
+        self.server_port = server_port
+    
+    def connect(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((self.server_host, self.server_port))
+        print("Connect Successfully!")
+
+    def send_message(self, msg):
+        msg = myencoder(msg)
+        self.sock.send(msg)
+        print("Send Message Successfully!")
+
+    def receive_message(self):
+        data = self.sock.recv(1024)
+        data = data.decode('utf-8')
+        print(data)
