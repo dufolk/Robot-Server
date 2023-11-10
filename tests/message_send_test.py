240b199d19abc7e46c9f1d1437c9ff66e2a41066
yhy <362316907@qq.com>
2023-11-10 21:52:38 +0800

    client更新向服务器发送自身坐标功能

--- tests/message_send_test.py
+++ tests/message_send_test.py
@@ -1,15 +1,21 @@
 from models import RobotEntity
 import sys
 import time
+import socket
 sys.path.append('.')
 from src.utils import *
 from configs import *
 import json
 if __name__ == '__main__':
-    client = RobotEntity(Config.SERVER_IP, Config.SERVER_PORT, 1)
-    while True:
-        msg = client.location.location
-        client.send2message(2, 'Hello')
-        time.sleep()
+    try:
+        client = RobotEntity(Config.SERVER_IP, Config.SERVER_PORT, 1)
+        while True:
+            msg = client.location
+            # client.send2message(2, 'Hello')
+            time.sleep(1)
+    except KeyboardInterrupt:
+        client.sock.shutdown(socket.SHUT_RDWR)
+        client.sock.close()
+        
 
     # print("Finished!")
--- tests/models/robot.py
+++ tests/models/robot.py
@@ -8,13 +8,14 @@ from configs import *
 from src.models import Location
 from src.utils import *
 
-class RobotEntity(ClientModel):
+class RobotEntity(ClientModel, Location):
     def __init__(self, server_ip, server_port, id:int):
-        super().__init__(server_ip, server_port)
+        ClientModel.__init__(self, server_ip, server_port) 
+        Location.__init__(self)
         self.id = id
         self.robot_connect()
         self.global_location = dict()
-        self.location = Location()
+        self.lock = threading.Lock()
         self.receive_thread = threading.Thread(target=self.recv_msg)
         self.receive_thread.setDaemon(True)
         self.receive_thread.start()
@@ -30,11 +31,29 @@ class RobotEntity(ClientModel):
                 msg = mydecoder(self.sock.recv(msg_len))
                 if msg == None:
                     continue
-                print(msg)
                 self.task(json.loads(msg))  
+            except Exception as e:
+                print('recv_msg:'+e)
+                continue
+    
+    def send_msg(self):
+        i=0
+        while True:
+            try:
+                # loc_msg = {"id": self.id, "location": self.location}
+                loc_msg = {"id": self.id, "location": [i,i,i]}
+                loc_msg = myencoder(json.dumps(loc_msg))
+                self.lock.acquire()
+                try:
+                    self.sock.send(loc_msg)
+                finally:
+                    self.lock.release()
+                time.sleep(1)
             except Exception as e:
                 print(e)
                 continue
+            i+=1
+        # pass
 
     def robot_connect(self):
         self.connect()
@@ -45,8 +64,10 @@ class RobotEntity(ClientModel):
         type = msg_type(msg)
         if type == Config.MSG_TYPE['LOCATION']:
             self.global_location = msg
+            print(self.global_location)
         # TODO: 用于根据报文类型进行不同的处理
         else:
+            # print(msg)
             pass
 
 # 向其他机器人发送消息
@@ -57,8 +78,12 @@ class RobotEntity(ClientModel):
             "msg": msg
         }
 
-        self.sock.send(myencoder(json.dumps(msg2robo)))
-        print("Send Message Successfully!")
+        msg2robo = myencoder(json.dumps(msg2robo))
+        self.lock.acquire()
+        try:
+            self.sock.send(msg2robo)
+        finally:
+            self.lock.release()
 
         
 
