7572c2f2b97e94f89fe56e7a87cd6229fe2d3795
yhy <362316907@qq.com>
2023-11-10 16:04:59 +0800

    client

--- tests/models/robot.py
+++ tests/models/robot.py
@@ -1,8 +1,9 @@
+# 
 import threading
 import time
 import sys
 sys.path.append('.')
-from client_model import ClientModel
+from .client_model import ClientModel
 from src.models import Location
 from src.utils import *
 
@@ -10,20 +11,39 @@ class RobotEntity(ClientModel):
     def __init__(self, server_ip, server_port):
         super().__init__(server_ip, server_port)
         self.connect()
+        self.global_location = dict()
         self.location = Location()
-        self.receive_thread = threading.Thread(target=self.recv_location)
+        self.receive_thread = threading.Thread(target=self.recv_msg)
         self.receive_thread.setDaemon(True)
         self.receive_thread.start()
 
-    def recv_location(self):
+    def recv_msg(self):
+        self.type = 1
         while True:
             try:
+                self.type = 1
                 msg_len = self.sock.recv(4).decode('utf-8')
-            except:
+                msg_len = int(msg_len)
+                self.msg = mydecoder(self.sock.recv(msg_len))
+                self.task()
+                time.sleep(0.05)
+            except Exception as e:
+                print(e)
                 continue
-            msg_len = int(msg_len)
-            msg = mydecoder(self.sock.recv(msg_len))
 
-            time.sleep(0.05)
+    def task(self):
+        type = msg_type(self.msg)
+        if type == 0:
+            self.global_location = json.loads(self.msg)
+            self.location.location = self.global_location[self.id]
+            print(self.location.location)
+        
+
+
+
+# {"id":[x,y,z], ...}
+# {"from":"id", "msg":"value"}
+            
+            
 
     
