a1d042c4506f649694160a830704aa5096cc0ff0
ren <renyuankuo@163.com>
2023-11-28 14:53:23 +0800

    前端（不冗余）

--- src/views/homepage.py
+++ src/views/homepage.py
@@ -5,27 +5,28 @@ import random
 from ..utils import *
 import time
 
+import threading
+
 views_blue = Blueprint('views', __name__)
 socketio = SocketIO()
-
+# 测试血量/速度/热值
+redblood=[[1.2,0,2000],[1.2,0,1940],[1.2,0,1880],[1.2,0,1820],[1.2,10,1760],[0,20,1760],[0,15,1760],[1.2,15,1760],[1.2,10,1760],[1.2,10,1760],[1.2,15,1700]]
+blueblood=[[1.0,30,2000],[0,40,2000],[0.7,50,1940],[1.0,50,1820],[0,40,1760],[0.5,35,1700],[0.5,35,1700],[0.2,40,1700],[0.2,45,940],[0,50,580],[0.4,55,520]]
+a=[0,0,2000]     
+b=[0,0,2000]  
+               
 # Author : ZP
 # 用于启动服务
 @views_blue.route('/')
 def index():
-    return render_template('test.html')
-
-# @socketio.on('message')
-# def message(msg):
-#     print(msg)
-#     loc = [random.randint(0, 100), random.randint(0, 100), random.randint(0, 100), random.randint(0, 100)]
-#     data = {'msg': loc}
-#     socketio.emit('message', data)
+    return render_template('index.html')
 
 @socketio.on('connect')
 def connect(message):
     print('connect')
     socketio.start_background_task(send_location)
-    socketio.emit('blood', 1)
+    socketio.start_background_task(blood_test)
+   
     
 def send_location():
     while True:
@@ -34,14 +35,17 @@ def send_location():
         socketio.emit('message', data)
         time.sleep(0.1)
 
-@socketio.on('blood_msg')
-def blood(message2):
-    print(message2['data'])
-    # socketio.start_background_task(blood_data)
-    
-def blood_data():
+# 测试robot传送数据
+def blood_test():
+    i=0
     while True:
-        data={'robot1':get_blood('robot1'),
-        'robot2':get_blood('robot2')}
-        socketio.emit('blood_message', data)
-        time.sleep(0.1)
+        a=redblood[i]
+        b=blueblood[i]
+        print(a)
+        print(b)
+        i=i+1
+        data_test=a
+        blue_test=b
+        socketio.emit('message_test', data_test)
+        socketio.emit('msg_blue', blue_test)
+        time.sleep(1)
