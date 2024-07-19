88694ddd486032fc1ad2409f39be37a54cd1a031
Dufolk <1589768970@qq.com>
2024-07-19 20:00:26 +0800

    sr

--- speechrecognition.py
+++ speechrecognition.py
@@ -8,48 +8,44 @@ import dashscope
 from dashscope import Generation
 from dashscope.api_entities.dashscope_response import Role
 
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("START")
-    audio = r.listen(source, timeout=3, phrase_time_limit=5)
-    print("OVER")
+# r = sr.Recognizer()
+# with sr.Microphone() as source:
+#     print("START")
+#     audio = r.listen(source, timeout=3, phrase_time_limit=5)
+#     print("OVER")
 
-# recognize speech using whisper
-try:
-    print("Whisper thinks you said: " + r.recognize_whisper(audio, model="small", language="chinese"))
-except sr.UnknownValueError:
-    print("Whisper could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Whisper")
+# # recognize speech using whisper
+# try:
+#     print("Whisper thinks you said: " + r.recognize_whisper(audio, model="small", language="chinese"))
+# except sr.UnknownValueError:
+#     print("Whisper could not understand audio")
+# except sr.RequestError as e:
+#     print("Could not request results from Whisper")
 
 def call_with_stream(question):
-    messages = [
-        {'role': 'user', 'content': f'{question}'}]
-    responses = Generation.call(
-        model='qwen-max-1201',
-        max_tokens=1500,
-        messages=messages,
-        result_format='message',
-        stream=True,
-        incremental_output=True
-    )
-    full_content = ''
-    for response in responses:
-        if response.status_code == HTTPStatus.OK:
-            full_content += response.output.choices[0]['message']['content']
-            api_reports(response, question)
-        else:
-            print('Request id: %s, Status code: %s, error code: %s, error message: %s' % (
-                response.request_id, response.status_code,
-                response.code, response.message
-            ))
-    print(f'{question}:\n\n' + full_content)
+    messages = [{'role': 'system', 'content': '到北边是模式1,到东边是模式2,到南边是模式3,到西边是模式4,逃跑是模式5,战斗及其他都是模式0.你需要根据我的语言表达判断我想要选择的模式，有时会有错别字，你需要根据相似的拼音判断我所说的内容。此外，我身处南边。你只需要给我返回模式数，不要给我返回任何其他内容。例1：输入：去北边战斗 返回：1 例2：输入：不要去西边，去动编 返回：2 例3：输入：过来 返回：3'},
+                {'role': 'user', 'content': '到我这来'}]
+    response = Generation.call(model="qwen-turbo",
+                               messages=messages,
+                               # 设置随机数种子seed，如果没有设置，则随机数种子默认为1234
+                               seed=random.randint(1, 10000),
+                               temperature=0.8,
+                               top_p=0.8,
+                               top_k=50,
+                               # 将输出设置为"message"格式
+                               result_format='message')
+    if response.status_code == HTTPStatus.OK:
+        content = response['output']['choices'][0]['message']['content']
+        print(content)
+        return content
+    else:
+        print('Request id: %s, Status code: %s, error code: %s, error message: %s' % (
+            response.request_id, response.status_code,
+            response.code, response.message
+        ))
 
-def api_reports(output_response, text):
-    time_now = time.strftime('%Y年%m月%d日%H点%M分%S秒', time.localtime())
-    f = open(f'大模型调用记录/通义千文/{text}_{time_now}.json', 'w')
-    output_json = json.dumps(output_response)
-    f.write(output_json)
-    f.close()
 
-# key = os.getenv("DASHSCOPE_API_KEY")
+key = "sk-3d0c2dbbf211476e8d7cc3062ad33d45"
+dashscope.api_key = key
+text = '为什么我保存response的json文件，content为空'
+call_with_stream(text)
